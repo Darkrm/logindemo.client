f3f0b1a23a5cd01bea466727ad74b7b86f4428b8
Arbor <arbor@kit.app>
2018-08-17 01:22:33 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# logindemo.client
+# loginDemo client
+Client for the [login demo server](https://github.com/Darkrm/logindemo.server), it's written in JavaScript, html and css. It uses the fetch api, async/await, localStorage and Document.querySelector so a browser that is up-to-date with the latest web technologies is a must.
+
+This client is very versatile thanks to the advances in web development and that I decoupled it completely from the login server. You could serve it from a web server (nginx), load it into a native app via a web view, wrap it in a native web view (think cordova or the like), you can turn it into a desktop app with electron and much more but for this demo we'll stick to a web server.
+
+The only requirement is that your web server of choice be able to proxy http requests for /api to the login server or if you have [Docker Compose](https://docs.docker.com/compose/) it's all been [done for you](https://github.com/Darkrm/logindemo.docker).
+
+A seed account is added to the database at runtime to allow you to login.
+
+login : test@test.com<br>
+password : test
+
+![Screenshot](/../screenshot/screen.png?raw=true "screenshot")
+
+## License
+
+The contents of this repository is not licensed out. You may not run, copy, modify or use anypart of it without the express written consent of the copyright holder.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ public/js/app.js
@@ -0,0 +1,97 @@
+(async () => {
+
+    var token = localStorage.getItem("jwt");
+
+    var res = await fetch(`api/session/check`, {
+        headers: {
+            "jwt": token
+        }
+    });
+
+    var el;
+
+    if (res.ok) {
+
+        el = '#wrap1';
+
+        localStorage.setItem("jwt", res.headers.get("jwtToken"));
+
+    } else {
+
+        el = '#wrap';
+
+    }
+
+    var div = document.querySelector(el);
+    div.style.display = "initial";
+
+})();
+
+var form = document.querySelector('#log');
+
+form.addEventListener('submit', async function() {
+    event.preventDefault();
+
+    var v = new FormData(this);
+    var data = {};
+
+    for (const [key, value] of v.entries()) {
+        data[key] = value;
+    }
+
+    var res = await fetch(`api/log/in`, {
+        method: "post",
+        body: JSON.stringify(data)
+    });
+
+    if (!res.ok) {
+
+        var div = document.querySelector('#err');
+        div.style.display = "initial";
+
+        setTimeout(function() {
+            div.style.display = "none";
+        }, 3000);
+
+        return;
+
+    }
+
+    localStorage.setItem("jwt", res.headers.get("jwtToken"));
+
+    var div = document.querySelector('#wrap1');
+
+    div.style.display = "initial";
+
+    var div = document.querySelector('#wrap');
+
+    div.style.display = "none";
+
+    form.reset();
+
+});
+
+var form2 = document.querySelector('#out');
+
+form2.addEventListener('submit', async function() {
+    event.preventDefault();
+
+    var token = localStorage.getItem("jwt");
+
+    var res = await fetch(`api/log/out`, {
+        headers: {
+            "jwt": token
+        }
+    });
+
+    localStorage.removeItem("jwt");
+
+    var div = document.querySelector('#wrap');
+
+    div.style.display = "initial";
+
+    var div = document.querySelector('#wrap1');
+
+    div.style.display = "none";
+
+});
